ffb522610028783bb3f45c6ba4026d01f0a4fe2e
Ataberk <88497592+Ataberkanit@users.noreply.github.com>
2022-03-09 17:56:05 +0300

    WhileForeach
    while foreach ders

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WhileForeachDersi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 den başlayarak consoledan girilen sayı kadar ortalama esaplayıp yazdıran program.
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<= sayi1)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi1);
+
+            // a dan z ye kadar olan sayıları yazdır.
+            char ch = 'a';
+            while (ch <= 'z')
+            {
+                Console.WriteLine(ch);
+                ch++;
+            }
+
+            Console.WriteLine("Foreach Örneği :");
+
+            string[] arabalar = { "BMW", "FORD", "TOYOTA", "RENAULT" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
