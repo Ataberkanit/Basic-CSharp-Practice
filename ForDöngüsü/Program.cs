27e85cd5cc6b528d96da513ade720870a610d80c
Ataberk <88497592+Ataberkanit@users.noreply.github.com>
2022-03-09 17:29:02 +0300

    ForDongusu
    For Döngüsü ders

---
+++ ForDöngüsü/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ForDöngüsüDersi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir Sayı giriniz.");
+            int sayac = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i + " tektir.");
+                }
+                else
+                {
+                    Console.WriteLine(i + " çifttir.");
+                }
+            }
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam = tekToplam + i;
+                else
+                    ciftToplam = ciftToplam + i;
+            }
+            Console.WriteLine("Tek sayıların toplamı : " + tekToplam + " Cift Sayıların Toplamı : " + ciftToplam);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break; // continue
+                }
+               
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
