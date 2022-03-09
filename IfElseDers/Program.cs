84a09d703695da0ea6f18b58b6efe387d99c99bd
Ataberk <88497592+Ataberkanit@users.noreply.github.com>
2022-03-09 16:54:27 +0300

    If_Else_Ders
    If-Else Ders

---
+++ IfElseDers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfElseDers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time<11)
+            {
+                Console.WriteLine("İyi günler.");
+            }
+            else if (time >18)
+            {
+                Console.WriteLine("İki Akşamlar.");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler.");
+            }
+            string sonuc = time <= 19 ? "İyi Günler" : "İyi geceler.";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+    }
+}
