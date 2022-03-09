7706dbcd412a958d6b99932c82340e98a04374f9
Ataberk <88497592+Ataberkanit@users.noreply.github.com>
2022-03-09 15:49:26 +0300

    Hata Yönetimi
    Hata yönetimi ders1

---
+++ HataYonetimi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girilen sayı : " + sayi);
+            }
+            catch (Exception ex)
+            {
+
+                throw; Console.WriteLine("Hata :" + ex.Message.ToString());
+            }
+            finally 
+            {
+                Console.WriteLine("İslem tamamlandı.");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                Console.WriteLine("Boş değer girdiniz!");
+                Console.WriteLine(ex);
+            }
+           
+        }
+    }
+}
